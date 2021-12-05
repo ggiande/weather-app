6557ac51cc871b5b6e934eca358f4efe0c9722bc
Giancarlo <ggarciadeleon@csustan.edu>
2021-12-05 00:40:59 -0800

    first commit

---
+++ app/src/main/java/com/technext/weatherapp/HttpRequest.java
@@ -0,0 +1,45 @@
+package com.technext.weatherapp;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class HttpRequest {
+    public static final String TAG = "HTTP REQUEST";
+    public static String executeGet(String targetURL) {
+        URL url;
+        HttpURLConnection connection = null;
+        try {
+            url = new URL(targetURL);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            InputStream is;
+            int status = connection.getResponseCode();
+            if (status != HttpURLConnection.HTTP_OK)
+                is = connection.getErrorStream();
+            else
+                is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            String line;
+            StringBuffer response = new StringBuffer();
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            Log.i(TAG, "Response: " + response);
+            return response.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+}
